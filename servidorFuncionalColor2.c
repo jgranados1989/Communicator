4ea8077d32fec48a078392a8fe7282736df77e3f
Fabian Fdz <fa.fdz@me.com>
2014-03-26 11:59:27 -0600

    Update

--- servidorFuncionalColor2.c
+++ servidorFuncionalColor2.c
@@ -11,15 +11,11 @@ void servidor(){
   	int sock;
   	char datosrecibidos[2048];    //arreglo para recibir los datos  
   	char datosenviados[2048];
-  	char nombreenviado[15];
+  	//char nombreenviado[15];
   	char nombrerecibido[15];
   	char datoColor[2048]="\x1b[34m";
   	struct sockaddr_in servidor; //struct obligatorio   para puertos y almacenar direcciones
   	int var1,misock;
-
-  	printf("digite su username: ");
-  	gets(nombreenviado);
-  	strcat(nombreenviado,":");
 
   	sock = socket(AF_INET, SOCK_STREAM, 0);  
   	int id;     
@@ -37,15 +33,14 @@ void servidor(){
  	bzero(&(servidor.sin_zero),8);
 
  	if (bind(sock,(struct sockaddr *)&servidor,sizeof(servidor))){
-  		perror("fallo el bind");
+  		perror("Fallo el bind");
   		exit(1);
  	}
 
  	listen(sock,5);
- 	//id=fork();
   	misock = accept(sock, (struct sockaddr *)0,0);
   	if (misock == -1){
-  		perror("error al aceptar");
+  		perror("Error al aceptar");
    	}
   	else{
 		while(1){
@@ -56,6 +51,9 @@ void servidor(){
 				return;
 			}
 			recv(misock,datosrecibidos,sizeof(datosrecibidos),0);
+			if (strcmp(datosrecibidos,NULL)==0){
+				return
+			}
             printf("%s",nombrerecibido);                 
 			printf("%s\n",datosrecibidos);
 
